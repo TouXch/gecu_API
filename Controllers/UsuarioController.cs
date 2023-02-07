10997f0b743a8fb623eb65d3f9836676d1918ac1
unknown <hanseltellez@gmail.com>
2023-02-07 10:08:49 -0500

    salva de progreso

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -35,6 +35,28 @@ namespace gecu_API.Controllers
             }
         }
 
+        //METODO PARA OBTENER UN USUARIO A PARTIR DE SU ID
+        [HttpGet]
+        [Route("listById/{idusuario:int}")]
+        public IActionResult listById(int idusuario)
+        {
+            Usuario usuario = _dbcontext.Usuarios.Find(idusuario);
+
+            if (usuario == null)
+            {
+                return BadRequest("No existe el usuario");
+            }
+
+            try
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = "ok", response = usuario });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
+            }
+        }
+
         //METODO PARA OBTENER LOS USUARIOS DE ACUERDO A UN ROL DETERMINADO
         [HttpGet]
         [Route("getByRol/{idrol:int}")]
@@ -72,6 +94,74 @@ namespace gecu_API.Controllers
                 {
                     return StatusCode(StatusCodes.Status200OK, new { message = "ok", response = usuarioRoles });
                 }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
+            }
+        }
+
+        //METODO PARA OBTENER SOLO LOS USUARIOS QUE TIENEN UN ROL ASIGNADO EN LA BD
+        [HttpGet]
+        [Route("listWithRole")]
+        public IActionResult listWithRole()
+        {
+            List<Usuario> list = new List<Usuario>();
+
+            try
+            {
+                list = _dbcontext.Usuarios.Where(u => u.IdRol != null).ToList();
+                return StatusCode(StatusCodes.Status200OK, new { message = "ok", response = list });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
+            }
+        }
+
+        //METODO PARA ASIGNAR UN ROL A UN USUARIO DETERMINADO
+        [HttpPut]
+        [Route("asignar/{idusuario:int}/{idrol:int}")]
+        public IActionResult asignar(int idusuario, int idrol)
+        {
+            Usuario usuario = _dbcontext.Usuarios.Find(idusuario);
+
+            if (usuario is null)
+            {
+                return BadRequest("El usuario no existe");
+            }
+
+            try
+            {
+                usuario.IdRol = idrol;
+                _dbcontext.Update(usuario);
+                _dbcontext.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK, new { message = "El rol ha sido asignado correctamente" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status200OK, new { message = ex.Message });
+            }
+        }
+
+        //METODO PARA QUITARLE UN ROL A UN USUARIO
+        [HttpPut]
+        [Route("quitRole/{idusuario:int}")]
+        public IActionResult quitRole(int idusuario)
+        {
+            Usuario usuario = _dbcontext.Usuarios.Find(idusuario);
+
+            if (usuario == null)
+            {
+                return BadRequest("No se encuentra el usuario especificado");
+            }
+
+            try
+            {
+                usuario.IdRol = null;
+                _dbcontext.Update(usuario);
+                _dbcontext.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK, new { message = "El usuario ya no cuenta con ningun rol" });
             }
             catch (Exception ex)
             {
